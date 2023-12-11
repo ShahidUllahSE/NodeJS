17a4eb3bc1ee1b361a837e73ed797824d0c1b1e7
ShahidUllahSE <shahidbangash8899@gmail.com>
2023-12-11 15:31:45 +0500

    average module

---
+++ CostumBackend/Modules/index.js
@@ -0,0 +1,5 @@
+const mod1 = require ("./mod1");
+
+console.log(mod1.avg([1,2,3]));
+
+console.log("this is index.js inside module folder")
---
+++ CostumBackend/Modules/mod1.js
@@ -0,0 +1,19 @@
+console.log("this is module");
+
+function average ( arr ){
+
+    let sum=0;
+    arr.forEach(element => {
+        sum+=element;
+        
+    });
+
+    return sum/arr.length
+
+}
+
+module.exports ={
+    
+    avg : average,
+    name :'shahid'
+}
